e1f8e082c97dea5ef95a391e161572904d062348
reiNine <reiNine@foxmail.com>
2023-01-08 23:08:25 +0800

    纠正部分问题，并添加了获取帖子标题的方法与请求头的cookie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from urllib import request
+import re
 
 
 class BaiduTieBa:
@@ -6,20 +7,40 @@ class BaiduTieBa:
         self.baseURL = baseURL
         self.seeLZ   = '?see_lz=' + str(seeLZ)
 
-    def fetchPage(self):
-        page     = request.urlopen(self.baseURL + self.seeLZ,)
-        response = page.read().decode('utf-8')
-        return response
+    # 根据url获取网页
+    def fetchPage(self, pageNum=1):
+        try:
+            url      = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
+            requests = request.Request(url, headers=headers)
+            response = request.urlopen(requests)
+            # response = request.urlopen(url)
+            html     = response.read().decode('utf-8')
+            return html
+        except:
+            print('连接失败')
+            return None
+
+    # 获取帖子的标题
+    def fetchTitle(self):
+        page    = self.fetchPage()
+        pattern = re.compile(r'(?<=<title>).*(?=_百度贴吧)')
+        result  = re.search(pattern, page)
+        if result:
+            return result.group()
+        else:
+            return f'获取标题失败'
 
 
 if __name__ == '__main__':
-    url      = 'https://tieba.baidu.com/p/6802656129'
-    headers  = {'User-Agent'     : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                   'Chrome/108.0.0.0 Safari/537.36',
-                'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5',
-                'Accept'         : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;'
-                                   'q=0.8,application/signed-exchange;v=b3;q=0.9'
-                }
-    requests = request.Request(url, headers=headers)
-    post01   = BaiduTieBa(url, 0)
-    print(post01.fetchPage())
+    originURL = 'https://tieba.baidu.com/p/6802656129'
+    headers   = {'User-Agent'     : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                    'Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54',
+                 'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5',
+                 'Accept'         : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;'
+                                    'q=0.8,application/signed-exchange;v=b3;q=0.9',
+                 'Cookie'         : 'BAIDUID=95312DF85A14760C5DA6F73C54207CCC:FG=1;'
+                                    'BIDUPSID=95312DF85A14760C5DA6F73C54207CCC; '
+                 }
+    post01    = BaiduTieBa(originURL, 0)
+    print(post01.fetchTitle())
+    # print(post01.fetchPage())
