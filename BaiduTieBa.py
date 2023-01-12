480189a708ccb7adf2754af2c3eb5201b85cd89c
reiNine <reiNine@foxmail.com>
2023-01-12 23:33:53 +0800

    删除了headers函数，新增注释

--- BaiduTieBa.py
+++ BaiduTieBa.py
@@ -1,7 +1,9 @@
+from headers import headers
 from urllib import request
 import re
 import time
 import random
+
 
 class BaiduTieBa:
     def __init__(self, baseURL, seeLZ):
@@ -12,7 +14,7 @@ class BaiduTieBa:
     def fetchPage(self, pageNum=1):
         try:
             url      = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
-            requests = request.Request(url, headers=headers)
+            requests = request.Request(url, headers=headers())
             response = request.urlopen(requests)
             html     = response.read().decode('utf-8')
             return html
@@ -41,20 +43,17 @@ class BaiduTieBa:
         else:
             return f'获取页数失败'
 
+    # 查看楼层信息
+    def fetchFloorInfo(self):
+        pass
 
+
+# 测试
 if __name__ == '__main__':
     originURL = 'https://tieba.baidu.com/p/8179625573?frwh=index'
-    headers   = {'User-Agent'     : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                                    'Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54',
-                 'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5',
-                 'Accept'         : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;'
-                                    'q=0.8,application/signed-exchange;v=b3;q=0.9',
-                 'Cookie'         : 'BAIDUID=95312DF85A14760C5DA6F73C54207CCC:FG=1;'
-                                    'BIDUPSID=95312DF85A14760C5DA6F73C54207CCC; '
-                 }
     post01    = BaiduTieBa(originURL, 0)
     method    = [post01.fetchTitle, post01.fetchTotalPage]
-    
+
     for i in method:
         print(i())
-        time.sleep(random.randrange(1,3) * 0.1)
+        time.sleep(random.randrange(1, 3) * 0.1)
