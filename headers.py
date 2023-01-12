dc093c53e8cab811793554f8016de5112f135859
reiNine <reiNine@foxmail.com>
2023-01-12 23:09:31 +0800

    将请求头单独分出一个文件

---
+++ headers.py
@@ -0,0 +1,20 @@
+# 请求头
+def headers():
+    header = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Cookie': 'XFI=cb8964a0-9288-11ed-952b-d7d81e092577; XFCS=A2913CEB55F41DDC2DDEC7EFB962A43A4E02A34278A8D279A0DA82535BB848DA; XFT=XiTlJt2iPHaOLqOT5hYi17TYpc1RWbCG++QcwT83NrI=; BAIDUID=95312DF85A14760C5DA6F73C54207CCC:FG=1; newlogin=1; BDUSS=JxflRwR0hPSjZaRGE5cnpqcE1FSUZHQ3N1bWdhWHJVQlFaS2lFbX4zYmhkZGRqRVFBQUFBJCQAAAAAAAAAAAEAAABxlczhcmVpTmluZV95AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOHor2Ph6K9jVl; BDUSS_BFESS=JxflRwR0hPSjZaRGE5cnpqcE1FSUZHQ3N1bWdhWHJVQlFaS2lFbX4zYmhkZGRqRVFBQUFBJCQAAAAAAAAAAAEAAABxlczhcmVpTmluZV95AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOHor2Ph6K9jVl; STOKEN=b10d6e5c343e97f3485a6576c9d6a4a2d3736f54e783d3586daadb3a39a0aff7; BIDUPSID=95312DF85A14760C5DA6F73C54207CCC; PSTM=1672476597; BAIDUID_BFESS=95312DF85A14760C5DA6F73C54207CCC:FG=1; ZFY=D4R2EOqWWA0Z5ZDfA3mvipt91XJ7gV7PfISoFSnGY8Y:C; BAIDU_WISE_UID=wapp_1673263961611_372; wise_device=0; USER_JUMP=-1; Hm_lvt_98b9d8c2fd6608d564bf2ac2ae642948=1672834404,1672913730,1672998736,1673531184; arialoadData=false; XFI=9c9a6760-927f-11ed-b102-6facbffa4ad2; XFCS=FF5D7AAC340DE12A7B9508EB9FCF1BC501B6F2D4E5B46D8659E9142388BDAC0D; XFT=6/QhSv4KXswNpeyH2xwTy0hkzSE687JNQkG4jmIW9LY=; BDRCVFR[QIvwdWWhfos]=mk3SLVN4HKm; delPer=0; PSINO=6; H_PS_PSSID=; BA_HECTOR=al05a4a18kaka0a5800421o11hs053e1l; BDORZ=FFFB88E999055A3F8A630C64834BD6D0; tb_as_data=e7ba442bacfe1a967b562d7a98abffcd73e2ecad3773a881d85238e2deeddb8941adaf549b29953928f2454b0cda8b73d2dc43ac82c1c6a851450389c51644bece3bb1f9dc0e337508d35befd03f236c8da13ac3db166167355c2df4d0a6b1a76b68a72a695e455a8971fb6e8e878b7a; Hm_lpvt_98b9d8c2fd6608d564bf2ac2ae642948=1673535174; RT="z=1&dm=baidu.com&si=b44b4cbd-4529-49cc-b8ac-67de0cac773d&ss=lct7etbv&sl=3&tt=2u5&bcn=https%3A%2F%2Ffclog.baidu.com%2Flog%2Fweirwood%3Ftype%3Dperf&ld=822m&ul=9a9s"',
+        'Referer': 'https://wappass.baidu.com/',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-site',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76',
+        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Microsoft Edge";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+    }
+    return header
