653fe8eb9a2922eeb34167b4a3d44b58dcdbebae
reiNine <reiNine@foxmail.com>
2023-01-12 23:30:46 +0800

    完成了fetchComments部分代码，新增注释

--- Posts.py
+++ Posts.py
@@ -1,5 +1,4 @@
 import BaiduTieBa as BDTB
-from urllib import request
 import re
 import time
 import random
@@ -9,20 +8,34 @@ class Posts(BDTB.BaiduTieBa):
     def __init__(self, baseURL, seeLZ):
         super().__init__(baseURL, seeLZ)
 
-    # 获取回复帖内容
+    # 获取回复帖内容(仅文字)
     def fetchComments(self):
-        page = BDTB.BaiduTieBa.fetchPage(self)
+        page                   = BDTB.BaiduTieBa.fetchPage(self)
+        extractPostPattern     = re.compile(r'(?<=content " style="display:;">\s{20}).*(?=</div><br>)')
+        rmLabelPattern         = re.compile(r'<.*>')
+        unextractedPostsList   = re.findall(extractPostPattern, page)
+        textList               = filter(lambda text: re.sub(rmLabelPattern, '', text), unextractedPostsList)
+        """ 相当于:
+        textList = []
+        for text in unextractedPostsList:
+            res = re.sub(rmLabelPattern, '', text)
+            testList.append(res)
+        """
+        return list(textList)
 
     # 获取楼中楼内容
     def fetchCommentsReply(self):
         pass
 
 
+# 测试
 if __name__ == '__main__':
-    originURL = 'https://tieba.baidu.com/p/8211167248?frwh=index'
+    originURL = 'https://tieba.baidu.com/p/8218780207?frwh=index'
     post01    = Posts(originURL, 0)
     method    = [post01.fetchTitle, post01.fetchTotalPage]
 
     for i in method:
         print(i())
         time.sleep(random.randrange(1, 3) * 0.1)
+    print()
+    print(post01.fetchComments())
