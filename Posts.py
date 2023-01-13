bdbcc83591d2539d820a547f4811824c2d6deaaa
reiNine <reiNine@foxmail.com>
2023-01-14 00:31:28 +0800

    修改了fetchComments方法

--- Posts.py
+++ Posts.py
@@ -1,27 +1,21 @@
-import BaiduTieBa as BDTB
+from BaiduTieBa import *
 import re
 import time
 import random
 
 
-class Posts(BDTB.BaiduTieBa):
+class Posts(BaiduTieBa, Tools):
     def __init__(self, baseURL, seeLZ):
         super().__init__(baseURL, seeLZ)
 
     # 获取回复帖内容(仅文字)
     def fetchComments(self):
-        page                   = BDTB.BaiduTieBa.fetchPage(self)
+        page                   = BaiduTieBa.fetchPage(self)
         extractPostPattern     = re.compile(r'(?<=content " style="display:;">\s{20}).*(?=</div><br>)')
-        rmLabelPattern         = re.compile(r'<.*>')
-        unextractedPostsList   = re.findall(extractPostPattern, page)
-        textList               = filter(lambda text: re.sub(rmLabelPattern, '', text), unextractedPostsList)
-        """ 相当于:
-        textList = []
-        for text in unextractedPostsList:
-            res = re.sub(rmLabelPattern, '', text)
-            testList.append(res)
-        """
-        return list(textList)
+        unextractedPostsList   = re.findall(extractPostPattern, page)  # 将初步筛选后的回复帖放进列表
+        for i in unextractedPostsList:
+            print(Tools.replace(self, i))
+            print('—————— 分割线 ——————')
 
     # 获取楼中楼内容
     def fetchCommentsReply(self):
@@ -30,12 +24,11 @@ class Posts(BDTB.BaiduTieBa):
 
 # 测试
 if __name__ == '__main__':
-    originURL = 'https://tieba.baidu.com/p/8218780207?frwh=index'
+    originURL = 'https://tieba.baidu.com/p/8101474840'
     post01    = Posts(originURL, 0)
-    method    = [post01.fetchTitle, post01.fetchTotalPage]
+    method    = [post01.fetchTitle, post01.fetchTotalPage, post01.fetchComments]
 
     for i in method:
         print(i())
         time.sleep(random.randrange(1, 3) * 0.1)
-    print()
-    print(post01.fetchComments())
+
