6651267f85e1f288d914db96de0c96e2a8eee74b
reiNine <reiNine@foxmail.com>
2023-01-16 01:16:12 +0800

    增添了Tools类功能，修改了fetchPage方法

--- BaiduTieBa.py
+++ BaiduTieBa.py
@@ -1,5 +1,7 @@
 from headers import headers
 from urllib import request
+import random
+import time
 import re
 
 
@@ -9,14 +11,20 @@ class Tools:
     # 保留图片
     replaceImgPrefix  = re.compile('<img class="BDE_Image".*?src="')
     replaceImgSuffix  = re.compile('" size=.*?>')
-    # 删除贴吧emoji
+    # 删除分隔符
+    removeDIV         = re.compile('<div>|</div>')
+    # 删除贴吧表情
     removeEmoji       = re.compile('<img class="BDE_Smiley".*?src=".*?" >')
+    # 删除装扮框
+    removeChatBox    = re.compile('<div class=.*?>')
 
     def replace(self, item):
-        item = re.sub(self.replaceBR, '\n', item)
-        item = re.sub(self.replaceImgPrefix, ' ', item)
-        item = re.sub(self.replaceImgSuffix, '', item)
-        item = re.sub(self.removeEmoji, '', item)
+        item = re.sub(self.replaceBR       , '\n', item)
+        item = re.sub(self.replaceImgPrefix, ' ' , item)
+        item = re.sub(self.replaceImgSuffix, ''  , item)
+        item = re.sub(self.removeDIV       , ''  , item)
+        item = re.sub(self.removeEmoji     , ''  , item)
+        item = re.sub(self.removeChatBox   , ''  , item)
         if item == '':
             return 'emoji'
         return item
@@ -28,20 +36,25 @@ class BaiduTieBa:
         self.seeLZ   = '?see_lz=' + str(seeLZ)
 
     # 根据url获取网页
-    def fetchPage(self, pageNum=1):
-        try:
-            url      = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
-            requests = request.Request(url, headers=headers())
-            response = request.urlopen(requests)
-            html     = response.read().decode('utf-8')
-            return html
-        except:
-            print('连接失败')
-            return None
+    def fetchPage(self, pageNum):
+        while True:
+            try:
+                url      = self.baseURL + self.seeLZ + '&pn=' + str(pageNum)
+                requests = request.Request(url, headers=headers())
+                response = request.urlopen(requests)
+                html     = response.read().decode('utf-8')
+                if '百度安全验证' in html:
+                    print('百度安全验证，正在重新获取网页...')
+                    time.sleep(random.randrange(2, 5) * 0.1)
+                else:
+                    return html
+            except:
+                print('连接失败')
+                return None
 
     # 获取帖子的标题
     def fetchTitle(self):
-        page    = self.fetchPage()
+        page    = self.fetchPage(1)
         pattern = re.compile(r'(?<=<title>).*(?=_百度贴吧)')
         result  = re.search(pattern, page)
         if result:
@@ -51,7 +64,7 @@ class BaiduTieBa:
 
     # 获取帖子的总页数
     def fetchTotalPage(self):
-        page    = self.fetchPage()
+        page    = self.fetchPage(1)
         pattern = re.compile(r'(?<=共<span class="red">)\d*(?=</span>页)')
         result  = re.search(pattern, page)
         if result:
@@ -69,6 +82,5 @@ class BaiduTieBa:
 if __name__ == '__main__':
     originURL = 'https://tieba.baidu.com/p/8101474840'
     post01    = BaiduTieBa(originURL, 0)
-    method    = [post01.fetchTitle, post01.fetchTotalPage]
 
-    print(post01.fetchPage())
+    print(post01.fetchPage(1))
