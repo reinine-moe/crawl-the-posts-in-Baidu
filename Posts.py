b78bd1f57569b823a930f2ae89945976b4db0131
reiNine <reiNine@foxmail.com>
2023-01-19 23:12:25 +0800

    修改了变量的表达式 #11，并添加了printAllComments方法 #25

--- Posts.py
+++ Posts.py
@@ -8,7 +8,8 @@ class Posts(BaiduTieBa, Tools):
 
     # 获取回复帖内容
     def fetchComments(self, page):
-        extractPostPattern     = re.compile(r'(?<=content " style="display:;">\s{20}).*(?=</div><br>)')
+        extractPostPattern     = re.compile(r'(?<=content " style="display:;">\s{20})|'
+                                            r'(?<=content\s\sclearfix" style="display:;">\s{12}).*(?=</div><br>)')
         unextractedPostsList   = re.findall(extractPostPattern, page)                    # 将初步筛选后的帖放进列表
         postsList              = [Tools.replace(self, p) for p in unextractedPostsList]  # 将完全筛选后的帖放进列表
         """ 测试 """
@@ -21,24 +22,15 @@ class Posts(BaiduTieBa, Tools):
     def fetchCommentsReply(self):
         pass
 
-
-# 测试
-if __name__ == '__main__':
-    originURL = 'https://tieba.baidu.com/p/8218759311?frwh=index'
-    post01    = Posts(originURL, 0)
-    method    = [post01.fetchTitle, post01.fetchTotalPage]
-    result    = []
-    for m in method:
-        result.append(m())
-    result.append(post01.fetchComments(post01.fetchPage(1)))
-
-    counter   = 1
-    print()
-    for i in result:
-        if isinstance(i, list):
-            for value in i:
-                print(f'—————— {counter}楼 ——————')
-                print(value)
+    # 打印所有回复帖
+    @staticmethod
+    def printAllComments(pageNum, post):
+        counter = 1
+        for i in range(1, pageNum + 1):
+            print(f'\n==================== 正在获取第{i}页数据 ====================')
+            page = post.fetchPage(i)
+            comments = post.fetchComments(page)
+            for comment in comments:
+                print(f'\n—————————— {counter}楼 ——————————')
+                print(comment)
                 counter += 1
-        else:
-            print(i, '\n')
