2c031e5ecba6cef2430b23bddf99e52c41a8c508
reiNine <reiNine@foxmail.com>
2023-02-05 19:03:48 +0800

    添加注释，并进行少许修改

--- main.py
+++ main.py
@@ -13,25 +13,29 @@ class Test:
 
     @staticmethod
     def isTrue(choice):
-        return True if choice == '1' or '0' else False
+        return True if choice in ('1', '0') else False
 
 
 # 主程序
 def start():
-    post        = Posts(originUrl, seeLZ)
-    initialPage = post.fetchPage(1)
-    pageNum     = int(post.fetchTotalPage(initialPage))
+    post        = Posts(originUrl, seeLZ)                # 保存信息
+    initialPage = post.fetchPage(1)                      # 获取首页信息
+    pageNum     = int(post.fetchTotalPage(initialPage))  # 获取总页数
 
-    print('帖子标题：'  , post.fetchTitle(initialPage))
-    print('帖子总页数：', pageNum)
+    print('\n\n帖子标题：', post.fetchTitle(initialPage))
+    print('帖子总页数：'  , pageNum)
 
     post.printAllComments(pageNum, post)
+
+    time.sleep(0.5)
+    input('\nenter any button to exit...')
 
 
 originUrl = input('请输入帖子地址：\n')
 seeLZ     = input('是否只获取楼主发言，是输入1，否输入0：\n')
 test      = Test()
 
+# 符合条件则开始运行
 if test.matchUrl(originUrl) and test.isTrue(seeLZ):
     start()
 else:
